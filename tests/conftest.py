3453c9de70c44626e50258ad9e39e45745233a5e
Simone Dalla <simodalla@gmail.com>
2024-02-25 19:48:06 +0100

    end working with async database

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,10 +5,9 @@ import pytest
 from fastapi.testclient import TestClient
 from httpx import AsyncClient
 
-from storeapi.routers.post import comment_table, post_table
-
 os.environ["ENV_STATE"] = "test"
 
+from storeapi.database import database  # noqa: E402
 from storeapi.main import app  # noqa: E402
 
 
@@ -24,9 +23,9 @@ def client() -> Generator:
 
 @pytest.fixture(autouse=True)
 async def db() -> AsyncGenerator:
-    post_table.clear()
-    comment_table.clear()
+    await database.connect()
     yield
+    await database.disconnect()
 
 
 @pytest.fixture()
