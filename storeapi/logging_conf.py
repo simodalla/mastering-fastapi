8dfcdb48fa6dde963ce129bc49001a8afe3714d0
Simone Dalla <simodalla@gmail.com>
2024-02-27 11:47:51 +0100

    loggin fast api

---
+++ storeapi/logging_conf.py
@@ -0,0 +1,50 @@
+from logging.config import dictConfig
+
+from storeapi.config import DevConfig, config
+
+
+def configure_logging() -> None:
+    dictConfig(
+        {
+            "version": 1,
+            "disable_existing_loggers": False,
+            "formatters": {
+                "console": {
+                    "class": "logging.Formatter",
+                    "datefmt": "%Y-%m-%dT%H:%M:%S",
+                    "format": "%(name)s:%(lineno)d - %(message)s",
+                },
+                "file": {
+                    "class": "logging.Formatter",
+                    "datefmt": "%Y-%m-%dT%H:%M:%S",
+                    "format": "%(asctime)s.%(msecs)03dZ | %(levelname)-8s | %(name)s:%(lineno)d - %(message)s",  # noqa: E501
+                },
+            },
+            "handlers": {
+                "default": {
+                    "class": "rich.logging.RichHandler",
+                    "level": "DEBUG",
+                    "formatter": "console",
+                },
+                "rotating_file": {
+                    "class": "logging.handlers.RotatingFileHandler",
+                    "level": "DEBUG",
+                    "formatter": "file",
+                    "filename": "storeapi.log",
+                    "maxBytes": 1024 * 1024 * 2,  # 2MB
+                    "backupCount": 5,
+                    "encoding": "utf8",
+                },
+            },
+            "loggers": {
+                "uvicorn": {"handlers": ["default", "rotating_file"], "level": "INFO"},
+                "storeapi": {
+                    "handlers": ["default", "rotating_file"],
+                    "level": "DEBUG" if isinstance(config, DevConfig) else "INFO",
+                    "propagate": False,
+                },
+                "databases": {"handlers": ["default"], "level": "WARNING"},
+                "aiosqlite": {"handlers": ["default"], "level": "WARNING"},
+            },
+        }
+    )
