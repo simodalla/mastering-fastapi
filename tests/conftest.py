43f817b7c86cb23a3c7e89ac7bbfd152a8756b67
Simone Dalla <simodalla@gmail.com>
2024-02-28 13:40:00 +0100

    Adding user registration and tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,7 @@ from httpx import AsyncClient
 
 os.environ["ENV_STATE"] = "test"
 
-from storeapi.database import database  # noqa: E402
+from storeapi.database import database, user_table  # noqa: E402
 from storeapi.main import app  # noqa: E402
 
 
@@ -32,3 +32,13 @@ async def db() -> AsyncGenerator:
 async def async_client(client) -> AsyncGenerator:
     async with AsyncClient(app=app, base_url=client.base_url) as ac:
         yield ac
+
+
+@pytest.fixture()
+async def registered_user(async_client: AsyncClient) -> dict:
+    user_details = {"email": "test@example.net", "password": "1234"}
+    await async_client.post("/register", json=user_details)
+    query = user_table.select().where(user_table.c.email == user_details["email"])
+    user = database.fetch_one(query)
+    user_details["id"] = user.id
+    return user_details
