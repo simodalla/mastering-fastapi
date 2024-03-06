58ec318e740ae6c5f3d82cc105715f1e949df085
Simone Dalla <simodalla@gmail.com>
2024-03-06 13:55:00 +0100

    Sending emails and testing with Python

---
+++ storeapi/tasks.py
@@ -0,0 +1,45 @@
+import logging
+
+import httpx
+
+from storeapi.config import config
+
+logger = logging.getLogger(__name__)
+
+
+class APIResponseError(Exception):
+    pass
+
+
+async def send_simple_email(to: str, subject: str, body: str):
+    logger.debug(f"Sending email to '{to[:3]}' with subject '{subject[:20]}'")
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.post(
+                f"https://api.mailgun.net/v3/{config.MAILGUN_DOMAIN}/messages",
+                auth=("api", config.MAIGUN_API_KEY),
+                data={
+                    "from": f"Simone Dalla <mailgun@{config.MAILGUN_DOMAIN}>",
+                    "to": [to],
+                    "subject": subject,
+                    "text": body,
+                },
+            )
+            response.raise_for_status()
+            logger.debug(response.content)
+            return response
+        except httpx.HTTPStatusError as err:
+            raise APIResponseError(
+                f"API request failed with status code {err.response.status_code}"
+            ) from err
+
+
+async def send_user_registration(email: str, confirmation_url: str):
+    return await send_simple_email(
+        email,
+        "Succesfully signed up",
+        (
+            f"Hi {email}! You have successfully signed up to Store REST API. Please confirm your"
+            f" email by clicking on the following link: {confirmation_url}"
+        ),
+    )
