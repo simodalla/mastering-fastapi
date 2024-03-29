705a6b6ac352848e87127f609fe710e8b5af2edc
Simone Dalla <simodalla@gmail.com>
2024-03-29 19:50:32 +0100

    update code to work with postgres

--- storeapi/database.py
+++ storeapi/database.py
@@ -44,13 +44,16 @@ like_table = sqlalchemy.Table(
     sqlalchemy.Column("user_id", sqlalchemy.ForeignKey("users.id"), nullable=False),
 )
 
+connect_args = {"check_same_thread": False} if "sqlite" in config.DATABASE_URL else {}
 engine = sqlalchemy.create_engine(
     config.DATABASE_URL,
-    connect_args={"check_same_thread": False},
+    connect_args=connect_args,
 )
 
 metadata.create_all(engine)
+db_args = {"min_size": 1, "max_size": 3} if "postgres" in config.DATABASE_URL else {}
 database = databases.Database(
     config.DATABASE_URL,  # type: ignore
     force_rollback=config.DB_FORCE_ROLL_BACK,
+    **db_args,
 )
